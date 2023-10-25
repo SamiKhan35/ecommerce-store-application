f95816fdc713c228b7ba3bd95e442c2f69c4d0f0
Sami Ul Haq <Sami_Khan@gmail.com>
2023-10-25 12:33:34 +0500

    Added Image Folde

--- BackendE-Store/utils/multerConfig.js
+++ BackendE-Store/utils/multerConfig.js
@@ -2,10 +2,11 @@ const multer = require('multer');
 const path = require('path');
 
 
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
 
-        cb(null, "Images");
+        cb(null, "Image");
 
 
     },
